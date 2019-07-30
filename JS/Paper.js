7fce930265cc836926c6d270a80fa2b9ba9f3747
chenxia <xchen01@best-inc.com>
2019-07-30 15:43:53 +0800

    增加叠纸官网

---
+++ JS/Paper.js
@@ -0,0 +1,52 @@
+var showDiv = document.getElementsByClassName('home-title');
+var link = document.getElementsByClassName('show');
+function overShow(){
+    showDiv[0].style.backgroundImage='url(img/logo_b_6d5db810_hover.png)';
+    link[0].style.visibility = 'visible';
+    link[1].style.visibility = 'visible';
+}
+function outHide(){
+    showDiv[0].style.backgroundImage='url(img/logo_b_6d5db810.png)';
+    link[0].style.visibility = 'hidden';
+    link[1].style.visibility = 'hidden';
+}
+function overShow2(){
+    showDiv[1].style.backgroundImage='url(img/logo_b_6d5db810_hover.png)';
+    link[2].style.visibility = 'visible';
+    link[3].style.visibility = 'visible';
+}
+function outHide2(){
+    showDiv[1].style.backgroundImage='url(img/logo_b_6d5db810.png)';
+    link[2].style.visibility = 'hidden';
+    link[3].style.visibility = 'hidden';
+}
+function overShow3(){
+    showDiv[2].style.backgroundImage='url(img/logo_b_6d5db810_hover.png)';
+    link[4].style.visibility = 'visible';
+    link[5].style.visibility = 'visible';
+}
+function outHide3(){
+    showDiv[2].style.backgroundImage='url(img/logo_b_6d5db810.png)';
+    link[4].style.visibility = 'hidden';
+    link[5].style.visibility = 'hidden';
+}
+function overShow4(){
+    showDiv[3].style.backgroundImage='url(img/logo_b_6d5db810_hover.png)';
+    link[6].style.visibility = 'visible';
+    link[7].style.visibility = 'visible';
+}
+function outHide4(){
+    showDiv[3].style.backgroundImage='url(img/logo_b_6d5db810.png)';
+    link[6].style.visibility = 'hidden';
+    link[7].style.visibility = 'hidden';
+}
+function overShow5(){
+    showDiv[4].style.backgroundImage='url(img/logo_b_6d5db810_hover.png)';
+    link[8].style.visibility = 'visible';
+    link[9].style.visibility = 'visible';
+}
+function outHide5(){
+    showDiv[4].style.backgroundImage='url(img/logo_b_6d5db810.png)';
+    link[8].style.visibility = 'hidden';
+    link[9].style.visibility = 'hidden';
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Paper
+叠纸官网模仿
+
+https://SnowdropK.github.io/Paper/index.html
